d8cbc63b62bcac16d71fcd701f891474c5589590
Barryrowe <barryrowe@gmail.com>
2016-01-03 17:31:44 -0500

    Configure Android Version
    Working Android version and configuration.

--- android/src/com/roaringcatgames/libgdxjam/android/AndroidLauncher.java
+++ android/src/com/roaringcatgames/libgdxjam/android/AndroidLauncher.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.badlogic.gdx.backends.android.surfaceview.ResolutionStrategy;
+import com.roaringcatgames.libgdxjam.App;
 import com.roaringcatgames.libgdxjam.LifeInSpace;
 
 public class AndroidLauncher extends AndroidApplication {
@@ -11,6 +13,7 @@ public class AndroidLauncher extends AndroidApplication {
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new LifeInSpace(), config);
+		config.useImmersiveMode = true;
+		initialize(App.Initialize(), config);
 	}
 }
