65de794b33dcf821c531fb3a7d794f316bcb26d3
Barryrowe <barryrowe@gmail.com>
2016-01-08 21:53:28 -0500

    Transition from Menu. ScreenWrap.
    Moved player and everything to SpaceScreen. Added click button to
    MenuScreen to transition into game. Added Working ScreenWrapSystem
    that works for Vertical, Horizontal directions.
    
    Should be refactored to just use velocity speed I think so it could be
    used on a player object for either direction actually.

--- core/src/com/roaringcatgames/libgdxjam/screens/MenuScreen.java
+++ core/src/com/roaringcatgames/libgdxjam/screens/MenuScreen.java
@@ -12,8 +12,15 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
+import com.roaringcatgames.kitten2d.ashley.components.BoundsComponent;
+import com.roaringcatgames.kitten2d.ashley.components.TextureComponent;
+import com.roaringcatgames.kitten2d.ashley.components.TransformComponent;
+import com.roaringcatgames.kitten2d.ashley.components.VelocityComponent;
 import com.roaringcatgames.kitten2d.ashley.systems.*;
 import com.roaringcatgames.libgdxjam.App;
+import com.roaringcatgames.libgdxjam.Assets;
+import com.roaringcatgames.libgdxjam.components.ScreenWrapComponent;
+import com.roaringcatgames.libgdxjam.components.ScreenWrapMode;
 import com.roaringcatgames.libgdxjam.systems.*;
 
 /**
@@ -28,7 +35,8 @@ public class MenuScreen extends LazyInitScreen implements InputProcessor {
     private Vector3 touchPoint;
     private Viewport viewport;
 
-    private Entity ball;
+
+    private Entity startGameButton;
 
     public MenuScreen(SpriteBatch batch, IScreenDispatcher dispatcher) {
         super();
@@ -67,17 +75,86 @@ public class MenuScreen extends LazyInitScreen implements InputProcessor {
         //Custom Systems
         Vector2 minBounds = new Vector2(0f, 0f);
         Vector2 maxBounds = new Vector2(cam.viewportWidth, cam.viewportHeight);
-        engine.addSystem(new PlayerSystem(playerPosition, 1f, cam));
-        engine.addSystem(new FiringSystem());
-        engine.addSystem(new CleanUpSystem(minBounds, maxBounds));
-        engine.addSystem(new RemainInBoundsSystem(minBounds, maxBounds));
-        engine.addSystem(new BulletSystem());
-        engine.addSystem(new FollowerSystem());
+        engine.addSystem(new ScreenWrapSystem(minBounds, maxBounds, App.PPM));
+//        engine.addSystem(new PlayerSystem(playerPosition, 1f, cam));
+//        engine.addSystem(new FiringSystem());
+//        engine.addSystem(new CleanUpSystem(minBounds, maxBounds));
+//        engine.addSystem(new RemainInBoundsSystem(minBounds, maxBounds));
+//        engine.addSystem(new BulletSystem());
+//        engine.addSystem(new FollowerSystem());
         //Extension Systems
         engine.addSystem(renderingSystem);
         //engine.addSystem(new GravitySystem(new Vector2(0f, -9.8f)));
         engine.addSystem(new DebugSystem(renderingSystem.getCamera(), Color.CYAN, Color.PINK, Input.Keys.TAB));
+
+
+        startGameButton = engine.createEntity();
+        startGameButton.add(BoundsComponent.create()
+                .setBounds(9.25f, 22f, 4f, 4f));
+        engine.addEntity(startGameButton);
+
+              testBackgrounds();
+
         App.game.multiplexer.addProcessor(this);
+    }
+
+    private void testBackgrounds(){
+        float speed = 10f;
+        Entity bg = engine.createEntity();
+        bg.add(TextureComponent.create()
+                .setRegion(Assets.getBgTile()));
+        bg.add(BoundsComponent.create()
+                .setBounds(0f, 0f, 16f, 16f));
+        bg.add(VelocityComponent.create()
+                .setSpeed(speed, 0f));
+        bg.add(ScreenWrapComponent.create()
+                .setMode(ScreenWrapMode.HORIZONTAL)
+                .setReversed(false));
+        bg.add(TransformComponent.create()
+                .setPosition(8f, 8f));
+        engine.addEntity(bg);
+
+        Entity bg2 = engine.createEntity();
+        bg2.add(TextureComponent.create()
+                .setRegion(Assets.getBgTile()));
+        bg2.add(BoundsComponent.create()
+                .setBounds(0f, 0f, 16f, 16f));
+        bg2.add(VelocityComponent.create()
+                .setSpeed(-speed, 0f));
+        bg2.add(ScreenWrapComponent.create()
+                .setMode(ScreenWrapMode.HORIZONTAL)
+                .setReversed(true));
+        bg2.add(TransformComponent.create()
+                .setPosition(16f, 8f));
+        engine.addEntity(bg2);
+
+        Entity bg3 = engine.createEntity();
+        bg3.add(TextureComponent.create()
+                .setRegion(Assets.getBgTile()));
+        bg3.add(BoundsComponent.create()
+                .setBounds(0f, 0f, 16f, 16f));
+        bg3.add(VelocityComponent.create()
+                .setSpeed(0f, speed));
+        bg3.add(ScreenWrapComponent.create()
+                .setMode(ScreenWrapMode.VERTICAL)
+                .setReversed(false));
+        bg3.add(TransformComponent.create()
+                .setPosition(10f, 16f));
+        engine.addEntity(bg3);
+
+        Entity bg4 = engine.createEntity();
+        bg4.add(TextureComponent.create()
+                .setRegion(Assets.getBgTile()));
+        bg4.add(BoundsComponent.create()
+                .setBounds(0f, 0f, 16f, 16f));
+        bg4.add(VelocityComponent.create()
+                .setSpeed(0f, -speed));
+        bg4.add(ScreenWrapComponent.create()
+                .setMode(ScreenWrapMode.VERTICAL)
+                .setReversed(true));
+        bg4.add(TransformComponent.create()
+                .setPosition(16f, 16f));
+        engine.addEntity(bg4);
     }
 
     /**************************
@@ -85,23 +162,12 @@ public class MenuScreen extends LazyInitScreen implements InputProcessor {
      **************************/
     @Override
     protected void update(float deltaChange) {
-
-
-
         engine.update(Math.min(deltaChange, App.MAX_DELTA_TICK));
-
-//        if(ball.getComponent(TransformComponent.class).position.y <= 1f){
-//            ball.getComponent(VelocityComponent.class).setSpeed(0f, 20f);
-//        }
     }
 
     @Override
     public void resize(int width, int height) {
         super.resize(width, height);
-        if(viewport != null) {
-            viewport.update(width, height);
-            cam.position.set(cam.viewportWidth / 2, cam.viewportHeight / 2, 0);
-        }
     }
     /**************************
      * Input Processor Methods
@@ -124,6 +190,12 @@ public class MenuScreen extends LazyInitScreen implements InputProcessor {
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        touchPoint.set(screenX, screenY, 0f);
+        touchPoint = cam.unproject(touchPoint);
+
+        if(startGameButton.getComponent(BoundsComponent.class).bounds.contains(touchPoint.x, touchPoint.y)){
+            dispatcher.endCurrentScreen();
+        }
         return false;
     }
 
