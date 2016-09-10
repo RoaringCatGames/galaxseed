e41c1e772c9f48e7d1fd4492db8a76785b794357
Barryrowe <barryrowe@gmail.com>
2016-09-09 23:23:29 -0400

    Lighten and Differ background Galaxy Spawns
    The galaxy spawns are now less frequent, adjust when resetting back to
    the top of the screen, and are set with an original random XY offset so
    they won't look as tiled/blocked.

--- core/src/com/roaringcatgames/galaxseed/systems/BackgroundSystem.java
+++ core/src/com/roaringcatgames/galaxseed/systems/BackgroundSystem.java
@@ -150,21 +150,21 @@ public class BackgroundSystem extends IteratingSystem {
                                                              Assets.getBgClearTileC();
                 Array<TextureRegion> galaxies = null;
                 float galaxyFloat = rnd.nextFloat();
-                if(galaxyFloat < 0.6f){
+                if(galaxyFloat < 0.3f){
                     galaxies = new Array<>();
-                    if(galaxyFloat < 0.3f){
+//                    if(galaxyFloat < 0.f){
                         galaxies.add(Assets.getGalaxyA());
                         galaxies.add(Assets.getGalaxyB());
                         galaxies.add(Assets.getGalaxyC());
 
-                    }else{
+//                    }else{
                         galaxies.add(Assets.getGasCluster());
                         galaxies.add(Assets.getGasClusterA());
                         galaxies.add(Assets.getGasClusterB());
                         galaxies.add(Assets.getGasClusterC());
                         galaxies.add(Assets.getGasClusterD());
                         galaxies.add(Assets.getGasClusterE());
-                    }
+//                    }
                 }
 
                 tiles.add(new BackgroundTile(x, y, rotation, texture, clearTile, galaxies));
@@ -184,11 +184,13 @@ public class BackgroundSystem extends IteratingSystem {
                 Entity galaxy = engine.createEntity();
                 int galaxyPos = rnd.nextInt(bg.galaxies.size);
                 float additionalOffest = K2MathUtil.getRandomInRange(0f, 3f);
+                float xOff = K2MathUtil.getRandomInRange(-1.5f, 1.5f);
+                float yOff = K2MathUtil.getRandomInRange(-1.5f, 1.5f);
                 galaxy.add(TextureComponent.create(engine)
                         .setRegion(bg.galaxies.get(galaxyPos)));
 
                 galaxy.add(TransformComponent.create(engine)
-                        .setPosition(bg.x, bg.y, Z.bg_galaxy)
+                        .setPosition(bg.x + xOff, bg.y + yOff, Z.bg_galaxy)
                         .setRotation(bg.rotation)
                         .setScale(1f, 1f));
 //                galaxy.add(BoundsComponent.create(engine)
@@ -199,6 +201,7 @@ public class BackgroundSystem extends IteratingSystem {
                         .setWrapOffset(offset + additionalOffest)
                         .shouldRandomPerpendicularPosition(true)
                         .setMinMaxPos(0f, right)
+                        .setRandomizeTexture(true)
                         .setPossibleRegions(bg.galaxies));
                 galaxy.add(VelocityComponent.create(engine)
                         .setSpeed(0f, bgSpeed));
