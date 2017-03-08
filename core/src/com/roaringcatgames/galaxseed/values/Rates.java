644f03cb435bf9f73e91c64d788a63cdf55632ed
Barryrowe <barryrowe@gmail.com>
2017-03-07 21:54:06 -0500

    Balance Helicopter firing rate down a bit.
    This should help balance the helicopter seeds a bit.

--- core/src/com/roaringcatgames/galaxseed/values/Rates.java
+++ core/src/com/roaringcatgames/galaxseed/values/Rates.java
@@ -11,8 +11,8 @@ public class Rates {
     public static final float SEED_GUN_BULLET_SPEED = 20f;
     public static final float SEED_GUN_GATLING_BULLET_SPEED = 30f;
 
-    public static final float HELI_GUN_BULLET_SPEED = 10f;
-    public static final float HELI_GUN_TIME_BETWEEN = 1f/2f;
+    public static final float HELI_GUN_BULLET_SPEED = 8f;
+    public static final float HELI_GUN_TIME_BETWEEN = 3f/4f;
 
     public static final float AURA_SLOWDOWN_RATE = 1f/6f;
 
