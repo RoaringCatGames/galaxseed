e4dc6df186e7a31b53aa819b9bf851cfd8fb0a90
Barryrowe <barryrowe@gmail.com>
2017-03-21 21:22:36 -0400

    Update latest BGM tracks
    The bgm tracks are now added.

--- core/src/com/roaringcatgames/galaxseed/systems/EnemyDamageUtil.java
+++ core/src/com/roaringcatgames/galaxseed/systems/EnemyDamageUtil.java
@@ -1,5 +1,6 @@
 package com.roaringcatgames.galaxseed.systems;
 
+import aurelienribon.tweenengine.Timeline;
 import aurelienribon.tweenengine.Tween;
 import aurelienribon.tweenengine.TweenEquations;
 import com.badlogic.ashley.core.Entity;
@@ -9,6 +10,7 @@ import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Vector2;
 import com.roaringcatgames.galaxseed.*;
+import com.roaringcatgames.galaxseed.components.WhenOffScreenComponent;
 import com.roaringcatgames.kitten2d.ashley.K2ComponentMappers;
 import com.roaringcatgames.kitten2d.ashley.K2EntityTweenAccessor;
 import com.roaringcatgames.kitten2d.ashley.VectorUtils;
@@ -131,81 +133,20 @@ public class EnemyDamageUtil {
         engine.addEntity(treeCover);
     }
 
-//    private static void generateHealthPack(PooledEngine engine, float x, float y, HealthPackType hType) {
-//        Entity healthPack = engine.createEntity();
-//        healthPack.add(TransformComponent.create(engine)
-//                .setPosition(x, y, Z.healthPack)
-//                .setScale(1f, 1f));
-//        healthPack.add(RotationComponent.create(engine)
-//                .setRotationSpeed(-60f));
-//        healthPack.add(TweenComponent.create(engine)
-//                .addTween(Tween.to(healthPack, K2EntityTweenAccessor.SCALE, 0.5f)
-//                        .target(1.25f, 1.25f)
-//                        .ease(TweenEquations.easeInOutBounce)
-//                        .repeatYoyo(Tween.INFINITY, 0)));
-//        healthPack.add(VelocityComponent.create(engine)
-//                .setSpeed(0f, 1f)); //TODO: use configured healthpack speed
-//        healthPack.add(TextureComponent.create(engine));
-//        Animation ani;
-//        TextureRegion glowRegion;
-//        float health;
-//        if(hType == HealthPackType.FERTILIZER){
-//            ani = Animations.getHealthFertilizer();
-//            glowRegion = Assets.getFertilizerGlow();
-//            health = Health.HealthPackFertilizer;
-//        }else{
-//            ani = Animations.getHealthWaterCan();
-//            glowRegion = Assets.getWaterCanGlow();
-//            health = Health.HealthPackWaterCan;
-//        }
-//        healthPack.add(AnimationComponent.create(engine)
-//                .addAnimation("DEFAULT", ani));
-//        healthPack.add(StateComponent.create(engine)
-//                .set("DEFAULT")
-//                .setLooping(true));
-//        healthPack.add(HealthPackComponent.create(engine)
-//                .setHealth(health)
-//                .setInstant(true));
-//        healthPack.add(BoundsComponent.create(engine)
-//                .setBounds(0f, 0f, 1f, 1f));
-//        engine.addEntity(healthPack);
-//
-//        Entity glow = engine.createEntity();
-//        glow.add(TransformComponent.create(engine)
-//                .setPosition(x, y, Z.healthGlow));
-//        glow.add(TextureComponent.create(engine)
-//                .setRegion(glowRegion));
-//        glow.add(FollowerComponent.create(engine)
-//                .setTarget(healthPack));
-//        glow.add(TweenComponent.create(engine)
-//                .addTween(Tween.to(glow, K2EntityTweenAccessor.COLOR, 0.5f)
-//                        .target(Colors.GLOW_YELLOW.r, Colors.GLOW_YELLOW.g, Colors.GLOW_YELLOW.b)
-//                        .ease(TweenEquations.easeInOutBounce)
-//                        .repeatYoyo(Tween.INFINITY, 0))
-//                .addTween(Tween.to(glow, K2EntityTweenAccessor.SCALE, 0.5f)
-//                        .target(1.25f, 1.25f)
-//                        .ease(TweenEquations.easeInOutBounce)
-//                        .repeatYoyo(Tween.INFINITY, 0)));
-//        engine.addEntity(glow);
-//    }
-
     private static void generateUpgradePowerUp(PooledEngine engine, float x, float y){
 
         Entity powerUp = engine.createEntity();
         powerUp.add(TransformComponent.create(engine)
                 .setPosition(x, y, Z.powerUp)
                 .setScale(1f, 1f));
-        powerUp.add(TweenComponent.create(engine)
-                .addTween(Tween.to(powerUp, K2EntityTweenAccessor.SCALE, 0.5f)
-                        .target(1.25f, 1.25f)
-                        .ease(TweenEquations.easeInOutBounce)
-                        .repeatYoyo(Tween.INFINITY, 0)));
+
         powerUp.add(VelocityComponent.create(engine)
                 .setSpeed(0f, healthPackSpeed));
         powerUp.add(TextureComponent.create(engine));
 
         powerUp.add(AnimationComponent.create(engine)
                 .addAnimation("DEFAULT", Animations.getUpgrade()));
+        powerUp.add(WhenOffScreenComponent.create(engine));
         powerUp.add(StateComponent.create(engine)
                 .set("DEFAULT")
                 .setLooping(true));
@@ -219,10 +160,9 @@ public class EnemyDamageUtil {
 
     public static void attachPlant(PooledEngine engine, Circle damageSource, Entity enemy, boolean isComet) {
 
-        //CircleBoundsComponent bb = K2ComponentMappers.circleBounds.get(damageSource);
         CircleBoundsComponent eb = K2ComponentMappers.circleBounds.get(enemy);
         TransformComponent et = K2ComponentMappers.transform.get(enemy);
-        bulletPos.set(damageSource.x, damageSource.y);//bb.circle.x, bb.circle.y);
+        bulletPos.set(damageSource.x, damageSource.y);
         enemyPos.set(eb.circle.x, eb.circle.y);
 
         Vector2 outVec;
